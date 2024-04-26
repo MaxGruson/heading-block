04deaea1bd03b3f361314504eb887489dd75d4d1
MaxGruson <max@gruson.studio>
2024-04-26 18:58:22 +0200

    Don't create subheading element if empty

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '218e4f20d14f2d2bbecf');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '485be5139e0346fdd8ed');
--- build/render.php
+++ build/render.php
@@ -16,5 +16,7 @@ $subheading = $attributes['subheading'] ?? '';
 
 <hgroup <?php echo get_block_wrapper_attributes(); ?>>
 	<h2><?php echo wp_kses_post( $heading ); ?></h2>
+	<?php if ( ! empty( $subheading ) ) { ?>
 	<h3><?php echo wp_kses_post( $subheading ); ?></h3>
+	<?php } ?>
 </hgroup>
